c2b3c378c77dbbd84b894c5c835607d157feb6b9
EmbryonicDev <95647817+EmbryonicDev@users.noreply.github.com>
2022-01-05 13:00:08 +0100

    Solution 1 Added

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+// Original Data
+// const removeFromArray = function() {
 
+// };
+
+// Solution 1
+const removeFromArray = function(...args) {
+  const array = args[0];
+  const newArray = [];
+  array.forEach((element) => {
+    if(!args.includes(element)) {
+      newArray.push(element);
+    }
+  });
+  return newArray;
 };
 
 // Do not edit below this line
