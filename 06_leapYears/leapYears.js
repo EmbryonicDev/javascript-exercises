39f0f2b49e9e414722f88ab8f4939f684a59a12f
EmbryonicDev <95647817+EmbryonicDev@users.noreply.github.com>
2022-01-05 17:15:19 +0100

    Completed exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
+// Original Data
+// const leapYears = function() {
 
+// };
+
+// Solution 1
+const leapYears = function(year) {
+  return ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0));
 };
 
 // Do not edit below this line
