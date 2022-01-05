4790f5a20e8ea010ee35b33d6efd7e181b44bb1b
EmbryonicDev <95647817+EmbryonicDev@users.noreply.github.com>
2022-01-05 09:27:05 +0100

    + 2 more solutions

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -3,17 +3,45 @@
 
 // };
 
-// // Do not edit below this line
-// module.exports = repeatString;
 
+// // My solution 1: 
+// const repeatString = function(string, num) {
+//   if(num < 0) return 'ERROR'
+//   let word = ''
+//   for(let i = 0; i < num; i++) {
+//     word += string
+//   }
+//   return word
+// };
+
+// My solution 2:
 const repeatString = function(string, num) {
   if(num < 0) return 'ERROR'
-  let word = ''
-  for(let i = 0; i < num; i++) {
-    word += string
+  let word = "";
+  while (num > 0) {
+    
+    word += string;
+    num--;
   }
-  return word
-};
+  return word;
+}
 
-// Do not edit below this line
+
+// // Solution 3 -> do while loop won't work here as it always runs through the loop at least once.
+// const repeatString = function(string, num) {
+//   let word = "";
+//   if(num < 0) return 'ERROR'
+//   do {
+//     word += string;
+//     num--;
+    
+//   } while(num > 0);
+//   return word;
+//   }
+
+
+// // Do not edit below this line
 module.exports = repeatString;
+
+
+
