9eeb58bdf60bfaf5f625d5cae821b226eb191540
EmbryonicDev <95647817+EmbryonicDev@users.noreply.github.com>
2022-01-05 13:44:29 +0100

    + Solution 2

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -3,17 +3,24 @@
 
 // };
 
-// Solution 1
-const removeFromArray = function(...args) {
-  const array = args[0];
-  const newArray = [];
-  array.forEach((element) => {
-    if(!args.includes(element)) {
-      newArray.push(element);
-    }
-  });
-  return newArray;
-};
+// // Solution 1
+// const removeFromArray = function(...args) {
+//   const array = args[0];
+//   const newArray = [];
+//   array.forEach((element) => {
+//     if(!args.includes(element)) {
+//       newArray.push(element);
+//     }
+//   });
+//   return newArray;
+// };
+
+// Solution 2
+  const removeFromArray = function(...args) {
+    const array = args[0]
+    return array.filter(element => !args.includes(element))
+  }
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
