107eb66fb99be53f1d4e69a5284ee1a2569cb893
EmbryonicDev <95647817+EmbryonicDev@users.noreply.github.com>
2022-01-06 06:21:34 +0100

    + formula to convert Fah to Cel

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,18 @@
-const ftoc = function() {
+// // Original Data
+// const ftoc = function() {
 
+// };
+
+// const ctof = function() {
+
+// };
+
+const ftoc = function(fah) {
+  let celsius = Math.round(((5/9)*(fah-32)) * 10 ) / 10;
+  return celsius;
 };
 
-const ctof = function() {
+const ctof = function(toFar) {
 
 };
 
