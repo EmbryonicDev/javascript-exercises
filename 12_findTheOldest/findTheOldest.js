79181ab93ef8f2abef0cb0c44a958b57e9da1b8b
EmbryonicDev <95647817+EmbryonicDev@users.noreply.github.com>
2022-01-19 15:20:49 +0100

    complete 12 challenge

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+  return array.reduce((acc, cur) => {
+    const oldestAge = getAge(acc.yearOfDeath, acc.yearOfBirth);
+    const nextAge = getAge(cur.yearOfDeath, cur.yearOfBirth);
+    return oldestAge < nextAge ? cur : acc;
+  })
 };
+
+const getAge = function(death, birth) {
+  if(!death) death = new Date().getFullYear();
+  return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
