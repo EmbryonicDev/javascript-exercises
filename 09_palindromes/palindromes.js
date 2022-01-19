fa86eeb71d110a1367bae88454d2ac4cec1edea8
EmbryonicDev <95647817+EmbryonicDev@users.noreply.github.com>
2022-01-19 07:39:06 +0100

    complete palindromes challenge

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function(str) {
+  const regex = /[\s!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+  let stripString = str.replace(regex, '').toLowerCase();
+  return (
+    stripString
+    .split('')
+    .reverse()
+    .join('') == stripString
+  )
 };
 
 // Do not edit below this line
