a404105275dadc87760106f862c71cce03b6a6d1
EmbryonicDev <95647817+EmbryonicDev@users.noreply.github.com>
2022-01-19 07:39:34 +0100

    complete fibonacci challenge

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(member) {
+  let a = 1, b = 1, c = '';
+  if(member < 0) return "OOPS";
+  if(member == 1 || member == 2) return 1;
+  for(i = 3; i <= member; i++) {
+    c = a + b;
+    a = b;
+    b = c;
+  }
+  return c
 };
 
 // Do not edit below this line
